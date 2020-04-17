f77dd2d197e3ebd88ce589d75ad39b6679335cb6
Usuario <Usuario@Usuario-VAIO>
2020-04-17 13:15:07 +0200

    Creada tablas

--- src/main/java/es/andresp/cartasfifa/CartaJugadores.java
+++ src/main/java/es/andresp/cartasfifa/CartaJugadores.java
@@ -1,9 +1,35 @@
 package es.andresp.cartasfifa;
 
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class CartaJugadores {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("es.andresp_CartasFifa_jar_1.0-SNAPSHOTPU");
+        EntityManager em = emf.createEntityManager();
+        
+        Nacionalidad nacionalidadPortugal = new Nacionalidad(1, "Portugal");
+        Nacionalidad nacionalidadSuecia = new Nacionalidad(2, "Suecia");
+        Nacionalidad nacionalidadHolanda = new Nacionalidad(3, "Holanda");
+        Jugadores jugadoresRonaldo = new Jugadores(1,"Cristiano", "Ronaldo", 1, 1985/02/05, 93, 764.5, FALSE);
+
+        em.getTransaction().begin();
+        em.persist(nacionalidadPortugal);
+        em.persist(nacionalidadSuecia);
+        em.persist(nacionalidadHolanda);
+        em.persist(jugadoresRonaldo);
+        em.getTransaction().commit();
+        
+        em.close(); 
+        emf.close(); 
+        try { 
+            DriverManager.getConnection("jdbc:derby:C:\\Users\\Usuario\\Documents\\NetBeansProjects\\CartasFifa\\BDCartasFifa;create=true"); 
+        } catch (SQLException ex) { 
+        }
     }
     
 }
